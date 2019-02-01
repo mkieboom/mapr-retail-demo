3ab9293b48987615bf3e5fb0c8b0da41dc74f3be
mkieboom <mkieboom@mapr.com>
2019-02-01 18:05:47 +0100

    Initial commit

--- raspberrypi/install.sh
+++ raspberrypi/install.sh
@@ -9,6 +9,36 @@ export RPI_GITHUB_PROJECT_URL="https://github.com/mkieboom/$RPI_GITHUB_PROJECT_N
 # The config file
 export RPI_CONFIG_FILE="/root/mapr-retail-demo/raspberrypi/mapr-retail-demo.config"
 
+# Install git
+apt-get -qq -y install git
+
+# Clone the raspberry-deployment ansible project
+cd ~/
+if [ ! -d "$RPI_GITHUB_PROJECT_NAME" ]; then
+	# github project not yet there, get it using git clone
+	git clone $RPI_GITHUB_PROJECT_URL
+else
+	# github project already there, update it using git pull
+	cd ~/$RPI_GITHUB_PROJECT_NAME
+	git pull $RPI_GITHUB_PROJECT_URL
+fi
+
+# Check if the config file is present
+if [ ! -f $RPI_CONFIG_FILE ];
+then
+	echo
+	echo "#############################################################"
+	echo "Config file not found. We need to configure that first, eg:"
+	echo "cp $RPI_CONFIG_FILE.template $RPI_CONFIG_FILE"
+	echo ""
+	echo "Edit the config file and provide all the details:"
+	echo "vi $RPI_CONFIG_FILE"
+	echo ""
+	echo "Once done, launch this command again:"
+	echo "curl https://raw.githubusercontent.com/mkieboom/mapr-retail-demo/master/raspberrypi/install.sh|bash"
+	exit 0;
+fi
+
 # Check if mkpasswd is installed, otherwise install it
 if [ ! -f /usr/bin/mkpasswd ];
 then
@@ -32,30 +62,6 @@ apt-get clean all
 # Install ansible, git and vim
 apt-get -y install ansible git vim whois
 
-# Clone the raspberry-deployment ansible project
-cd ~/
-if [ ! -d "$RPI_GITHUB_PROJECT_NAME" ]; then
-	# github project not yet there, get it using git clone
-	git clone $RPI_GITHUB_PROJECT_URL
-else
-	# github project already there, update it using git pull
-	cd ~/$RPI_GITHUB_PROJECT_NAME
-	git pull $RPI_GITHUB_PROJECT_URL
-fi
-
-# Check if the config file is present
-if [ ! -f $RPI_CONFIG_FILE ];
-then
-	echo
-	echo "Config file not found. Copy: $RPI_CONFIG_FILE.template, eg:"
-	echo "cp $RPI_CONFIG_FILE.template $RPI_CONFIG_FILE"
-	echo "and provide the details:"
-	echo "vi $RPI_CONFIG_FILE"
-	echo ""
-	echo "Once done, launch this command again:"
-	echo "curl https://raw.githubusercontent.com/mkieboom/mapr-retail-demo/master/raspberrypi/install.sh|bash"
-	exit 0;
-fi
 
 
 # Run the ansible playbook
